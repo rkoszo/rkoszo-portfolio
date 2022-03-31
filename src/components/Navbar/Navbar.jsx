93fc9a8c82f6205e008b59c39dc1c9e22ad2fbc0
Kószó Ramón <koszo.ramon@gmail.com>
2022-03-31 22:40:04 +0200

    Implementation: setup navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 import './Navbar.scss';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='app__navbar'>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'skills', 'works', 'contact'].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
